194903fc9eccff88b2ebbe2490d2169c497853c4
hankyul9540 <117982781+hankyul12@users.noreply.github.com>
2024-11-28 14:29:43 +0900

    상세 보기 추가 (미완

--- src/main/java/db/BookDAO.java
+++ src/main/java/db/BookDAO.java
@@ -30,4 +30,40 @@ public class BookDAO {
 
         return temp;
     }
+    public String getBook_Num(String id) throws SQLException {
+        String query = "SELECT b.*, a.Name FROM Author a " +
+                       "JOIN Writing w ON a.Author_ID = w.Author_ID " +
+                       "JOIN Book b ON w.Book_ID = b.Book_ID " +
+                       "WHERE w.Book_ID = ?";
+        
+        String temp = "";
+        
+        // 연결, PreparedStatement, ResultSet을 사용하여 쿼리 실행
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
+            
+            // id를 파라미터로 설정
+            pstmt.setString(1, id);
+            
+            try (ResultSet resultSet = pstmt.executeQuery()) {
+                if (resultSet.next()) {
+                    int bookId = resultSet.getInt("Book_ID");
+                    String title = resultSet.getString("Title");
+                    String genre = resultSet.getString("Genre");
+                    int quantity = resultSet.getInt("Quantity");
+                    int publicationYear = resultSet.getInt("Publication_Year");
+                    int publisherId = resultSet.getInt("Publisher_ID");
+                    String img_book = resultSet.getString("Img_Book");
+                    String author = resultSet.getString("Name");
+                    
+                    // 결과를 temp 문자열에 저장
+                    temp = bookId + "&" + title + "&" + genre + "&" + quantity + "&" + publicationYear + "&" +
+                           publisherId + "&" + img_book + "&" + author;
+                }
+            }
+        }
+        
+        return temp;
+    }
+
 }
